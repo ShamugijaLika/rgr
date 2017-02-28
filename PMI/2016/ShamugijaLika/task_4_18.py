ea017935a60af9add28c454e66a1af517427d877
ShamugijaLika <shelley.bass.li@gmail.com>
2017-02-28 23:13:46 +0300

    Create task_4_18.py

---
+++ PMI/2016/ShamugijaLika/task_4_18.py
@@ -0,0 +1,12 @@
+name = 'Янис Плиекшанс более известен, как поэт Райнис.'
+place = 'Дунавская волость, Латвия'
+year_live = 1865
+year_dead = 1929
+age = year_dead - year_live
+interest = 'Культура'
+print(name)
+print("Место рождения: ", place)
+print("Год рождения: ", year_live, "\nГод смерти:", year_dead)
+print("Возраст на момент смерти: ", age)
+print("Область интрересов: ", interest)
+input("\n\nPress Enter for Exit")
